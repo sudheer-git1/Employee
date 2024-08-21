227385cf020557d91880e784963d3387e85f2974
sudheer-git1 <sudheernagavarapu9866@gmail.com>
2024-08-21 12:23:25 +0530

    Update EmployeeRepository.java

--- src/main/java/com/employee/repo/EmployeeRepository.java
+++ src/main/java/com/employee/repo/EmployeeRepository.java
@@ -3,7 +3,7 @@ package com.employee.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.employee.model.Employee;
-
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
